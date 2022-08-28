cf44bdbbd732200275ba0cb01b172307f22c240e
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-08-28 19:57:44 +0200

    new commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // ...
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  }
+  build: {
+    commonjsOptions: {
+        transformMixedEsModules: true,
+    },
+},
+plugins: [react()],
+resolve: {
+    alias: [
+        {
+            find: /^jss-plugin-(.*)$/,
+            replacement: "$1",
+            customResolver: (id) => {
+                if (id === "{}") {
+                    id = "global";
+                }
+                return resolve(__dirname, `./node_modules/jss-plugin-${id}/src/index.js`);
+            },
+        },
+    ],
+},
 })
