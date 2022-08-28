1fe5deedf6814a6623c4a2ca2a9692eb3827a858
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-08-28 17:40:06 +0200

    new commit

--- src/Sections/Navbar/Navbar.jsx
+++ src/Sections/Navbar/Navbar.jsx
@@ -4,24 +4,33 @@ import { Typography } from "@mui/material"
 import "./Navbar.css"
 
 import menu from "../../assets/menu.png"
+import { useState } from "react"
 
 const Navbar = () => {
+  const [open, setOpen] = useState(true)
+
+  const handleClick = () => {
+    if (open) {
+      setOpen(false)
+    } else {
+      setOpen(true)
+    }
+  }
+
   return (
     <div className="navbar">
       <div className="logo">
-        <Typography variant="span" sx={{fontWeight: 800,fontSize: "1.5rem",lineHeight: "1.875rem",}}>DonationApp</Typography>
+        <Typography variant="span" sx={{ fontWeight: 800, fontSize: "1.5rem", lineHeight: "1.875rem", }}>DonationApp</Typography>
       </div>
-      <div className="links">
-        <ul>
-          <li><a>About</a></li>
-          <li><a>Contact</a></li>
-          <li><a>Features</a></li>
-        </ul>
-      </div>
-      <div>
-        <button className="nav-btn">Get Started</button>
-      </div>
-      <div className="menu">
+        <div className={open ? "links" : "mobileLinks"}>
+          <ul>
+            <li><a>About</a></li>
+            <li><a>Contact</a></li>
+            <li><a>Features</a></li>
+          </ul>
+            <button className="nav-btn">Get Started</button>
+        </div>
+      <div className="menu" onClick={handleClick}>
         <img src={menu} alt="" />
       </div>
     </div>
